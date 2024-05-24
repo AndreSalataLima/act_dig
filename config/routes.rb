b13422a8bb0d14916da77dfcb6d331f36c12f6dd
Andrre Lima <andre.salatalima@gmail.com>
2024-05-24 16:54:22 +0200

    css fixed for title

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'pages#landing_page'
 
   # Non-nested routes
-  get 'pages/home'
-  get 'pages/dashboard'
-  get 'pages/home2'
+  # get 'pages/home'
+  # get 'pages/dashboard'
+  # get 'pages/home2'
   get 'pages/landing_page'
-  get 'session_histories/index'
-  get 'session_histories/show'
-  get 'sensors/show'
-  get 'sensors/edit'
-  get 'pages/index'
+  # get 'session_histories/index'
+  # get 'session_histories/show'
+  # get 'sensors/show'
+  # get 'sensors/edit'
+  # get 'pages/index'
 
   # Nested routes for users, sessions, exercises
-  resources :users do
-    resources :sessions do
-      resources :exercises do
-        resources :sensors, only: [:show, :edit]
-      end
-      resources :session_histories, only: [:index, :show]
-    end
-  end
+  # resources :users do
+    # resources :sessions do
+      # resources :exercises do
+        # resources :sensors, only: [:show, :edit]
+      # end
+      # resources :session_histories, only: [:index, :show]
+    # end
+  # end
 
   # Rota de verificação de saúde
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 end
